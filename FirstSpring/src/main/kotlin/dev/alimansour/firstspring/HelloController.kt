14f32c85261aee81109c0ecd30ae9684bf82341b
Ali Mansour <dev.ali.mansour@gmail.com>
2026-08-07 14:47:43 +0300

    feat: add POST endpoint to HelloController and change server port to 8080
    - Add @PostMapping("/hello") endpoint accepting a request body string in HelloController
    - Update server.port in application.properties to 8080

--- FirstSpring/src/main/kotlin/dev/alimansour/firstspring/HelloController.kt
+++ FirstSpring/src/main/kotlin/dev/alimansour/firstspring/HelloController.kt
@@ -1,10 +1,15 @@
 package dev.alimansour.firstspring
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class HelloController {
     @GetMapping("/hello")
     fun hello() = "Hello World!"
+
+    @PostMapping("/hello")
+    fun helloPost(@RequestBody name: String) = "Hello $name!"
 }
