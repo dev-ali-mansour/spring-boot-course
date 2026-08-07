3feae32f8c8e01d6e4806a35a8e9acc031165018
Ali Mansour <dev.ali.mansour@gmail.com>
2026-08-07 17:39:53 +0300

    feat: Create Post API in CategoryController to create a category

--- sb-ecom/src/main/kotlin/dev/alimansour/sbecom/controller/CategoryController.kt
+++ sb-ecom/src/main/kotlin/dev/alimansour/sbecom/controller/CategoryController.kt
@@ -2,12 +2,20 @@ package dev.alimansour.sbecom.controller
 
 import dev.alimansour.sbecom.model.Category
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class CategoryController {
-    private val categories: List<Category> = mutableListOf()
+    private val categories: MutableList<Category> = mutableListOf()
 
     @GetMapping("/api/public/categories")
     fun getCategories(): List<Category> = categories
+
+    @PostMapping("/api/public/categories")
+    fun createCategory(@RequestBody category: Category): String {
+        categories.add(category)
+        return "Category added successfully!"
+    }
 }
