543ccf694ff0510e575a6f45a96f306c58a8b133
Ali Mansour <dev.ali.mansour@gmail.com>
2026-08-07 17:23:03 +0300

    feat: initialize sb-ecom Spring Boot project
    Initialize the Maven wrapper, configuration files, and main Kotlin application entry point for sb-ecom.

---
+++ sb-ecom/src/main/kotlin/dev/alimansour/sbecom/SbEcomApplication.kt
@@ -0,0 +1,11 @@
+package dev.alimansour.sbecom
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SbEcomApplication
+
+fun main(args: Array<String>) {
+    runApplication<SbEcomApplication>(*args)
+}
