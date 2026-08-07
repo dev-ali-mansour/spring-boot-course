2388757b0162c0930329999acc704df5e0b99714
Ali Mansour <dev.ali.mansour@gmail.com>
2026-08-07 14:24:59 +0300

    feat: implement HelloController REST endpoint and set server port
    - Add HelloController REST controller with /hello GET endpoint returning "Hello World!"
    - Update application.properties to set server.port to 9090

---
+++ FirstSpring/src/main/kotlin/dev/alimansour/firstspring/HelloController.kt
@@ -0,0 +1,10 @@
+package dev.alimansour.firstspring
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HelloController {
+    @GetMapping("/hello")
+    fun hello() = "Hello World!"
+}
