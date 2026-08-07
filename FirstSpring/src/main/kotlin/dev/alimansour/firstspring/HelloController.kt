629351847d197ec69e9a17061258dea659420895
Ali Mansour <dev.ali.mansour@gmail.com>
2026-08-07 15:04:32 +0300

    feat: add path variable GET endpoint to HelloController
    - Add @GetMapping("/hello/{name}") endpoint using @PathVariable in HelloController

--- FirstSpring/src/main/kotlin/dev/alimansour/firstspring/HelloController.kt
+++ FirstSpring/src/main/kotlin/dev/alimansour/firstspring/HelloController.kt
@@ -1,6 +1,7 @@
 package dev.alimansour.firstspring
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
@@ -8,8 +9,14 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class HelloController {
     @GetMapping("/hello")
-    fun hello(): HelloResponse = HelloResponse(message = "Hello, World!")
+    fun hello(): HelloResponse =
+        HelloResponse(message = "Hello, World!")
+
+    @GetMapping("/hello/{name}")
+    fun helloParam(@PathVariable name: String): HelloResponse =
+        HelloResponse(message = "Hello, $name!")
 
     @PostMapping("/hello")
-    fun helloPost(@RequestBody name: String): HelloResponse = HelloResponse(message = "Hello, $name!")
+    fun helloPost(@RequestBody name: String): HelloResponse =
+        HelloResponse(message = "Hello, $name!")
 }
