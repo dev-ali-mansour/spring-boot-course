fd01578cce8d1bf0c618f5c1b59e12bd6947e325
Ali Mansour <dev.ali.mansour@gmail.com>
2026-08-07 12:48:31 +0300

    feat: add @Qualifier annotation for bean autowiring in Manager
    Use @Qualifier("employee") alongside @Autowired to explicitly specify the target bean for dependency injection in Manager.

--- SpringExample/src/main/kotlin/com/example/autowire/annotation/Manager.kt
+++ SpringExample/src/main/kotlin/com/example/autowire/annotation/Manager.kt
@@ -1,11 +1,13 @@
 package com.example.autowire.annotation
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Component
 
 @Component
-class Manager(@Autowired private val employee: Employee) {
+class Manager(@Autowired @Qualifier("employee") private val employee: Employee) {
 //    @Autowired
+//    @Qualifier("employee")
 //    lateinit var employee: Employee
 
     override fun toString(): String {
