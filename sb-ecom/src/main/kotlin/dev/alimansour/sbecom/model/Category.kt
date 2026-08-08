7c4da8274492c7a5c356fad5075822e3ae4aae5c
Ali Mansour <dev.ali.mansour@gmail.com>
2026-08-08 12:15:31 +0300

    feat: configure identity strategy for Category ID generation
    Add @GeneratedValue(strategy = GenerationType.IDENTITY) to Category entity ID and add JPA SQL debug configuration comments in application.properties.

--- sb-ecom/src/main/kotlin/dev/alimansour/sbecom/model/Category.kt
+++ sb-ecom/src/main/kotlin/dev/alimansour/sbecom/model/Category.kt
@@ -1,11 +1,14 @@
 package dev.alimansour.sbecom.model
 
 import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 
 @Entity(name = "categories")
 class Category(
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long = 0,
     var name: String = "",
 )
