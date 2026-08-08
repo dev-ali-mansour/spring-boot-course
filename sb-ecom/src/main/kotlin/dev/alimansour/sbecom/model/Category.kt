f237efc0ffdbf1f24a2c7f520da491c03d04c096
Ali Mansour <dev.ali.mansour@gmail.com>
2026-08-08 18:19:46 +0300

    feat: add minimum length Size validation to Category name
    Annotate Category name with @Size(min = 5) constraint and custom error message.

--- sb-ecom/src/main/kotlin/dev/alimansour/sbecom/model/Category.kt
+++ sb-ecom/src/main/kotlin/dev/alimansour/sbecom/model/Category.kt
@@ -5,6 +5,7 @@ import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Size
 
 @Entity(name = "categories")
 class Category(
@@ -13,5 +14,6 @@ class Category(
     var id: Long = 0,
 
     @NotBlank
+    @Size(min = 5, message = "Category name must be at least 5 characters")
     var name: String = "",
 )
