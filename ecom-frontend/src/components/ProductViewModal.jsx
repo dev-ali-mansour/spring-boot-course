3954d39bc39c96f1ff30daf324da44b5390aab60
Ali Mansour <dev.ali.mansour@gmail.com>
2026-08-31 01:17:56 +0300

    feat: add ProductViewModal component for detailed product display in a modal

---
+++ ecom-frontend/src/components/ProductViewModal.jsx
@@ -0,0 +1,90 @@
+import {Dialog, DialogBackdrop, DialogPanel, DialogTitle} from '@headlessui/react'
+import {Divider} from "@mui/material";
+import Status from "./Status.jsx";
+import {MdClose, MdDone} from "react-icons/md";
+
+export default function ProductViewModal({isOpen, setIsOpen, product, isAvailable}) {
+
+    function close() {
+        setIsOpen(false)
+    }
+
+    return (<>
+        <Dialog open={isOpen} as="div" className="relative z-10" onClose={close} __demoMode>
+            <DialogBackdrop className="fixed inset-0 bg-gray-500 opacity-75 transition-opacity duration-300"/>
+            <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+                <div className="flex min-h-full items-center justify-center p-4">
+                    <DialogPanel
+                        transition
+                        className="relative transform overflow-hidden rounded-lg bg-white shadow-xl trasition-all md:max-w-155 md:min-w-155 w-full"
+                    >
+                        {product.image && (
+                            <div className="flex justify-center aspect-3/2">
+                                <img
+                                    src={product.image}
+                                    alt={product.name}
+                                ></img>
+                            </div>
+                        )}
+
+                        <div className="px-6 pt-10 pb-2">
+                            <DialogTitle as="h1"
+                                         className="lg:text-3xl sm:text-2xl text-xl font-semibold leading-6 text-gray-800 mb-4">
+                                {product.name}
+                            </DialogTitle>
+
+                            <div className="space-y-2 text-gray-700 pb-4">
+                                <div className="flex items-center justify-between gap-2">
+                                    {product.specialPrice ? (
+                                        <div className="flex items-center gap-2">
+                      <span className="text-gray-400 line-through">
+                        ${Number(product.price).toFixed(2)}
+                      </span>
+                                            <span className="sm:text-xl font-semibold text-slate-700">
+                        ${Number(product.specialPrice).toFixed(2)}
+                      </span>
+                                        </div>
+                                    ) : (
+                                        <span className="text-xl font-bold">
+                      {" "}
+                                            ${Number(product.price).toFixed(2)}
+                    </span>
+                                    )}
+
+                                    {isAvailable ? (
+                                        <Status
+                                            text="In Stock"
+                                            icon={MdDone}
+                                            backgroundColor="bg-teal-200"
+                                            textColor="text-teal-900"
+                                        />
+                                    ) : (
+                                        <Status
+                                            text="Out-Of-Stock"
+                                            icon={MdClose}
+                                            backgroundColor="bg-rose-200"
+                                            textColor="text-rose-700"
+                                        />
+                                    )}
+                                </div>
+
+                                <Divider/>
+
+                                <p>{product.description}</p>
+                            </div>
+                        </div>
+                        <div className="px-6 py-4 flex justify-end gap-4">
+                            <button
+                                onClick={close}
+                                type="button"
+                                className="px-4 py-2 text-sm font-semibold text-slate-700 border border-slate-700 hover:text-slate-800 hover:border-slate-800 rounded-md cursor-pointer"
+                            >
+                                Close
+                            </button>
+                        </div>
+                    </DialogPanel>
+                </div>
+            </div>
+        </Dialog>
+    </>)
+}
