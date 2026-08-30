f6fa799d43405b8fdab0d3205a8145c12ddb1213
Ali Mansour <dev.ali.mansour@gmail.com>
2026-08-30 11:22:00 +0300

    feat: add initial setup for e-commerce frontend with React, Vite, and Tailwind CSS
    - Added React and Vite configuration files.
    - Created main entry point for the application.
    - Included Tailwind CSS for styling.
    - Added SVG assets for React and Vite logos.

---
+++ ecom-frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { FaBaby, FaBan, FaBeer } from "react-icons/fa";
+import "./App.css";
+import { DiReact } from "react-icons/di";
+
+function App() {
+  return (
+    <h1 class="flex items-center justify-center h-screen bg-gray-800 text-white text-2xl font-bold">
+      Welcome <DiReact />
+    </h1>
+  );
+}
+
+export default App;
