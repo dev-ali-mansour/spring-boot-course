254e52a68e490b7faf0973ee835a9b69a018c570
Ali Mansour <dev.ali.mansour@gmail.com>
2026-08-31 03:16:56 +0300

    feat: add WebConfig to configure CORS for local development

---
+++ sb-ecom/src/main/kotlin/dev/alimansour/sbecom/security/WebConfig.kt
@@ -0,0 +1,20 @@
+package dev.alimansour.sbecom.security
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig(
+    @Value("\${frontend.url}")
+    private val frontendUrl: String,
+) : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:3000", frontendUrl)
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true);
+    }
+}
