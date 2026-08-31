1a644ebeced4dcd6776942b4b12fec624a45a0fd
Ali Mansour <dev.ali.mansour@gmail.com>
2026-08-31 22:40:39 +0300

    refactor: use useSelector for isLoading and errorMessage in Products component and remove commented code

--- ecom-frontend/src/components/Products.jsx
+++ ecom-frontend/src/components/Products.jsx
@@ -5,8 +5,7 @@ import {useEffect} from "react";
 import {fetchProducts} from "../store/actions/index.js";
 
 export default function Products() {
-    const isLoading = false;
-    const errorMessage = "";
+    const {isLoading, errorMessage} = useSelector((state) => state.errors);
     const {products} = useSelector((state) => state.products);
     const dispatch = useDispatch();
 
@@ -14,32 +13,6 @@ export default function Products() {
         dispatch(fetchProducts())
     }, [dispatch])
 
-
-
-    /*const products = [
-        {
-            id: 652,
-            name: "IPhone XS Max",
-            image: "https://placehold.co/600x400",
-            description:
-                "Experience the latest in mobile technology with advanced cameras, powerful processing, and all-day battery.",
-            quantity: 10,
-            price: 1450.0,
-            discount: 10.0,
-            specialPrice: 1305.0,
-        },
-        {
-            id: 654,
-            name: "MacBook Air M2s",
-            image: "https://placehold.co/600x400",
-            description:
-                "Ultra-thin laptop with Apple's M2 chip, providing fast performance in a lightweight, portable design.",
-            quantity: 0,
-            price: 2250.0,
-            discount: 20.0,
-            specialPrice: 2040.0,
-        },
-    ];*/
     return (
         <div className="lg:px-14 sm:px-8 px-4 py-14 2xl:w-[90%] 2xl:mx-auto">
             {isLoading ? (
