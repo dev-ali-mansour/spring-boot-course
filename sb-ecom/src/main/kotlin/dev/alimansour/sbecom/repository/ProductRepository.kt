c28712d7de25dd3fb2b7921ad1e9caa9b8a61509
Ali Mansour <dev.ali.mansour@gmail.com>
2026-09-01 04:16:36 +0300

    feat: add support for dynamic queries in ProductRepository with JpaSpecificationExecutor

--- sb-ecom/src/main/kotlin/dev/alimansour/sbecom/repository/ProductRepository.kt
+++ sb-ecom/src/main/kotlin/dev/alimansour/sbecom/repository/ProductRepository.kt
@@ -5,10 +5,11 @@ import dev.alimansour.sbecom.model.Product
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor
 import org.springframework.stereotype.Repository
 
 @Repository
-interface ProductRepository : JpaRepository<Product, Long> {
+interface ProductRepository : JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
     fun findByCategory(category: Category, pageable: Pageable): Page<Product>
     fun findByNameLikeIgnoreCase(name: String, pageable: Pageable): Page<Product>
 }
