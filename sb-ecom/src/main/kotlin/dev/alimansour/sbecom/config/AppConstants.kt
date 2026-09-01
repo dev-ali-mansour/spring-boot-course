0e544004b537c886fa9885578307c333deb304f6
Ali Mansour <dev.ali.mansour@gmail.com>
2026-09-01 12:37:26 +0300

    chore: adjust default PAGE_SIZE in AppConstants from 50 to 20 for pagination refinement

--- sb-ecom/src/main/kotlin/dev/alimansour/sbecom/config/AppConstants.kt
+++ sb-ecom/src/main/kotlin/dev/alimansour/sbecom/config/AppConstants.kt
@@ -2,7 +2,7 @@ package dev.alimansour.sbecom.config
 
 object AppConstants {
     const val PAGE_NUMBER: Int = 0
-    const val PAGE_SIZE: Int = 50
+    const val PAGE_SIZE: Int = 20
     const val SORT_PRODUCTS_BY: String = "price"
     const val SORT_CATEGORIES_BY: String = "id"
 }
