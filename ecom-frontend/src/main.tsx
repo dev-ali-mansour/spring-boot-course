ac319aa8102c25e77d8c92eafdfb756d128a7f62
Ali Mansour <dev.ali.mansour@gmail.com>
2026-09-02 13:34:38 +0300

    chore: setup TanStack Query and Zustand
    - Updates `package.json` dependencies.
    - Configures `QueryClientProvider` in `main.tsx`.

--- ecom-frontend/src/main.tsx
+++ ecom-frontend/src/main.tsx
@@ -2,13 +2,14 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {Provider} from "react-redux";
-import {store} from "./store/reducers/store";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root') as HTMLElement).render(
-    <Provider store={store}>
+    <QueryClientProvider client={queryClient}>
         <StrictMode>
             <App/>
         </StrictMode>
-    </Provider>
+    </QueryClientProvider>
 );
