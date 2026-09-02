570f066d33aa6de8a5f8260960c4a0e4bccf07e7
Ali Mansour <dev.ali.mansour@gmail.com>
2026-09-02 16:41:49 +0300

    feat: add placeholder image to constants and export for reuse

--- ecom-frontend/src/utils/constant.ts
+++ ecom-frontend/src/utils/constant.ts
@@ -1,8 +1,10 @@
+import placeholder from "../assets/placeholder.png"
 import bannerImageOne from "../assets/sliders/s_1.webp";
 import bannerImageTwo from "../assets/sliders/s_2.webp";
 import bannerImageThree from "../assets/sliders/s_3.webp";
 
 export {
+    placeholder,
     bannerImageOne,
     bannerImageTwo,
     bannerImageThree
