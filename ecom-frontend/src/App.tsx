466aa58a3659bc26ac45e432283ba6a142cf15f3
Ali Mansour <dev.ali.mansour@gmail.com>
2026-09-02 17:39:40 +0300

    feat: add Contact route to App and integrate Contact component

--- ecom-frontend/src/App.tsx
+++ ecom-frontend/src/App.tsx
@@ -4,6 +4,7 @@ import Home from "./components/home/Home.tsx";
 import Products from "./components/products/Products.tsx";
 import Navbar from "./components/shared/Navbar.tsx";
 import About from "./components/about/About.tsx";
+import Contact from "./components/contact/Contact.tsx";
 
 const App: React.FC = () => {
     return (
@@ -13,6 +14,7 @@ const App: React.FC = () => {
                 <Route path="/" element={<Home/>}/>
                 <Route path="/products" element={<Products/>}/>
                 <Route path="/about" element={<About/>}/>
+                <Route path="/contact" element={<Contact/>}/>
             </Routes>
         </Router>
     );
