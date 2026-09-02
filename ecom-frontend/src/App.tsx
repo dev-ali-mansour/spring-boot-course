ae9b0e38e91a8f2f1437e07362b6deea0f196e21
Ali Mansour <dev.ali.mansour@gmail.com>
2026-09-02 15:17:52 +0300

    feat: integrate Navbar component into App layout

--- ecom-frontend/src/App.tsx
+++ ecom-frontend/src/App.tsx
@@ -2,10 +2,12 @@ import * as React from "react";
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Home from "./components/home/Home.tsx";
 import Products from "./components/products/Products.tsx";
+import Navbar from "./components/shared/Navbar.tsx";
 
 const App: React.FC = () => {
     return (
         <Router>
+            <Navbar/>
             <Routes>
                 <Route path="/" element={<Home/>}/>
                 <Route path="/products" element={<Products/>}/>
