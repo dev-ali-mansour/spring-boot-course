703649e24f2c26886ffa8ec5c8509b8fde65f98d
Ali Mansour <dev.ali.mansour@gmail.com>
2026-09-03 23:10:19 +0300

    feat: add Address interface definition in TypeScript

---
+++ ecom-frontend/src/types/Address.ts
@@ -0,0 +1,9 @@
+export interface Address {
+    addressId?: number | string;
+    street?: string;
+    buildingName?: string;
+    city?: string;
+    state?: string;
+    pinCode?: string;
+    country?: string;
+}
