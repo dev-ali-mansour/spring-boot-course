538ce6ce05ece4d52002c728f665f8fa5bc12561
Ali Mansour <dev.ali.mansour@gmail.com>
2026-09-03 12:28:55 +0300

    feat: integrate react-hot-toast for notifications in App
    - Adds `Toaster` component with bottom-center alignment.

--- ecom-frontend/src/App.tsx
+++ ecom-frontend/src/App.tsx
@@ -5,18 +5,22 @@ import Products from "./components/products/Products.tsx";
 import Navbar from "./components/shared/Navbar.tsx";
 import About from "./components/about/About.tsx";
 import Contact from "./components/contact/Contact.tsx";
+import {Toaster} from "react-hot-toast";
 
 const App: React.FC = () => {
     return (
-        <Router>
-            <Navbar/>
-            <Routes>
-                <Route path="/" element={<Home/>}/>
-                <Route path="/products" element={<Products/>}/>
-                <Route path="/about" element={<About/>}/>
-                <Route path="/contact" element={<Contact/>}/>
-            </Routes>
-        </Router>
+        <React.Fragment>
+            <Router>
+                <Navbar/>
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/products" element={<Products/>}/>
+                    <Route path="/about" element={<About/>}/>
+                    <Route path="/contact" element={<Contact/>}/>
+                </Routes>
+            </Router>
+            <Toaster position={"bottom-center"}/>
+        </React.Fragment>
     );
 };
 
