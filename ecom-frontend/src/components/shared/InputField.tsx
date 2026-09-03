db7d16ee906dc276df3346e7c087fdbfcc7bf5da
Ali Mansour <dev.ali.mansour@gmail.com>
2026-09-04 00:46:28 +0300

    feat: create reusable InputField component with validation support
    - Adds a new `InputField` component that integrates with `react-hook-form`.
    - Supports validation for required fields, min length, email, and URL patterns.
    - Includes dynamic error handling and customizable props for flexibility.

---
+++ ecom-frontend/src/components/shared/InputField.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+import {FieldErrors, FieldValues, UseFormRegister} from "react-hook-form";
+
+export interface InputFieldProps {
+    label: string;
+    id: string;
+    type?: string;
+    errors: FieldErrors;
+    register: UseFormRegister<FieldValues>;
+    required?: boolean;
+    message?: string;
+    className?: string;
+    min?: number;
+    placeholder?: string;
+}
+
+const InputField: React.FC<InputFieldProps> = (
+    {
+        label,
+        id,
+        type,
+        errors,
+        register,
+        required,
+        message,
+        className,
+        min,
+        placeholder,
+    }: InputFieldProps
+) => {
+    return (
+        <div className={"flex flex-col gap-1 w-full"}>
+            <label
+                htmlFor={id}
+                className={`${className ? className : ""} font-semibold text-sm text-slate-800`}>
+                {label}
+            </label>
+
+            <input
+                type={type}
+                id={id}
+                placeholder={placeholder}
+                className={`${className ? className : ""} px-2 py-2 border outline-none bg-transparent 
+                text-slate-800 rounded-md ${errors[id]?.message ? "border-red-500" : "border-slate-700"}`}
+                {...register(id, {
+                        required: required ? {value: true, message: message || "This field is required"} : false,
+                        minLength: min
+                            ? {value: min, message: `Minimum ${min} character is required`}
+                            : undefined,
+                        pattern:
+                            type === "email"
+                                ? {
+                                    value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                                    message: "Invalid email"
+                                } : type === "url"
+                                    ? {
+                                        value: /^(https?:\/\/)?(([a-zA-Z0-9\u00a1-\uffff-]+\.)+[a-zA-Z\u00a1-\uffff]{2,})(:\d{2,5})?(\/[^\s]*)?$/,
+                                        message: "Please enter a valid URL"
+                                    } : undefined
+                    }
+                )}/>
+            {errors[id]?.message && (
+                <p className={"text-sm font=semibold text-red-600 mt-0"}>
+                    {errors[id]?.message as string}
+                </p>
+            )}
+        </div>
+    );
+};
+
+export default InputField;
