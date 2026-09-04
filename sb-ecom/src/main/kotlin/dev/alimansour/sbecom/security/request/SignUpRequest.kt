482c9d53d80081e31f4af998a531f08a505c936c
Ali Mansour <dev.ali.mansour@gmail.com>
2026-09-04 18:40:21 +0300

    chore: update validation rules in `SignUpRequest` data class for improved input constraints

--- sb-ecom/src/main/kotlin/dev/alimansour/sbecom/security/request/SignUpRequest.kt
+++ sb-ecom/src/main/kotlin/dev/alimansour/sbecom/security/request/SignUpRequest.kt
@@ -1,6 +1,9 @@
 package dev.alimansour.sbecom.security.request
 
-import jakarta.validation.constraints.*
+import jakarta.validation.constraints.Email
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Pattern
+import jakarta.validation.constraints.Size
 
 data class SignUpRequest(
     @NotBlank
@@ -10,20 +13,19 @@ data class SignUpRequest(
     @Size(min = 2, max = 30, message = "Username must be between 2 and 30 characters!")
     val lastName: String,
     @NotBlank
-    @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters!")
+    @Size(min = 4, max = 30, message = "Username must be between 4 and 30 characters!")
     val username: String,
 
     @NotBlank
-    @Max(value = 50, message = "Email must not be grater than 50 characters!")
+    @Size(max = 50, message = "Email must not be grater than 50 characters!")
     @Email
     val email: String,
 
     val roles: Set<String>?,
 
     @NotBlank
-    @Max(value = 120)
     @Pattern(
-        regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
+        regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,120}$",
         message = "Password must be at least 8 characters, contain at least one lowercase, at least one uppercase, at least one digit, and at least one special character!"
     )
     val password: String,
