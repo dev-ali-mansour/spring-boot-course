581cd2212b2eaac79eedbc9be48a637a19ca6339
Ali Mansour <dev.ali.mansour@gmail.com>
2026-09-04 14:34:07 +0300

    feat: add firstName and lastName fields to UserInfoResponse

--- sb-ecom/src/main/kotlin/dev/alimansour/sbecom/security/response/UserInfoResponse.kt
+++ sb-ecom/src/main/kotlin/dev/alimansour/sbecom/security/response/UserInfoResponse.kt
@@ -2,6 +2,8 @@ package dev.alimansour.sbecom.security.response
 
 data class UserInfoResponse(
     val id: Long?,
+    val firstName: String,
+    val lastName: String,
     val jwtToken: String? = null,
     val username: String,
     val roles: List<String>,
