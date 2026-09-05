15edfd45e08917a75ac2f3a68cde535d32bd027d
Ali Mansour <dev.ali.mansour@gmail.com>
2026-09-05 05:15:09 +0300

    feat: integrate React Query Devtools into main.tsx for enhanced debugging

--- ecom-frontend/src/main.tsx
+++ ecom-frontend/src/main.tsx
@@ -3,6 +3,7 @@ import {createRoot} from 'react-dom/client';
 import './index.css';
 import App from './App';
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
@@ -11,5 +12,6 @@ createRoot(document.getElementById('root') as HTMLElement).render(
         <StrictMode>
             <App/>
         </StrictMode>
+        <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
 );
