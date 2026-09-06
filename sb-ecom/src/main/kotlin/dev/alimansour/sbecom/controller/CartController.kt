2d15ab304244fb6d10927eabc2eb77bc52deda24
Ali Mansour <dev.ali.mansour@gmail.com>
2026-09-06 17:54:20 +0300

    feat: add createOrUpdateCart endpoint to manage carts with multiple items

--- sb-ecom/src/main/kotlin/dev/alimansour/sbecom/controller/CartController.kt
+++ sb-ecom/src/main/kotlin/dev/alimansour/sbecom/controller/CartController.kt
@@ -1,6 +1,7 @@
 package dev.alimansour.sbecom.controller
 
 import dev.alimansour.sbecom.payload.CartDTO
+import dev.alimansour.sbecom.payload.CartItemDTO
 import dev.alimansour.sbecom.service.CartService
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.HttpStatus
@@ -10,6 +11,15 @@ import org.springframework.web.bind.annotation.*
 @RestController
 @RequestMapping("/api")
 class CartController(private val cartService: CartService) {
+
+    @Tag(name = "Cart APIs", description = "APIs for managing carts")
+    @PostMapping("/carts/create")
+    fun createOrUpdateCart(
+        @RequestBody cartItems: List<CartItemDTO>
+    ): ResponseEntity<String> {
+        val response: String = cartService.createOrUpdateCartWithItems(cartItems)
+        return ResponseEntity(response, HttpStatus.CREATED)
+    }
 
     @Tag(name = "Cart APIs", description = "APIs for managing carts")
     @PostMapping("/carts/products/{productId}/quantity/{quantity}")
