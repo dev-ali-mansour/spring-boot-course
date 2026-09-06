936e6cf94cd1412e3c3da3fcf0db251926711f7b
Ali Mansour <dev.ali.mansour@gmail.com>
2026-09-06 14:10:22 +0300

    feat: enhance Checkout with address validation, error handling, and skeleton loader

--- ecom-frontend/src/components/checkout/Checkout.tsx
+++ ecom-frontend/src/components/checkout/Checkout.tsx
@@ -1,7 +1,10 @@
 import React, {useState} from "react";
-import {Step, StepLabel, Stepper} from "@mui/material";
+import {Button, Step, StepLabel, Stepper} from "@mui/material";
 import AddressInfo from "./AddressInfo.tsx";
-import {useGetUserAddresses} from "../../hooks/useQueries.ts";
+import {getErrorMessage, useGetUserAddresses} from "../../hooks/useQueries.ts";
+import {useAuthStore} from "../../store";
+import toast from "react-hot-toast";
+import Skeleton from "../shared/Skeleton.tsx";
 
 const steps = [
     "Address",
@@ -11,8 +14,27 @@ const steps = [
 ];
 
 const Checkout: React.FC = () => {
-    const [activeStep] = useState(0);
-    const { data: addresses = [] } = useGetUserAddresses();
+    const [activeStep, setActiveStep] = useState(0);
+    const {data: addresses = [], isLoading, error} = useGetUserAddresses();
+    const errorMessage = error ? getErrorMessage(error) : null;
+    const {selectedUserCheckoutAddress} = useAuthStore();
+    const paymentMethod = false;
+
+    const handleBack = () => {
+        setActiveStep((prevStep) => prevStep - 1);
+    };
+
+    const handleNext = () => {
+        if (activeStep === 0 && !selectedUserCheckoutAddress) {
+            toast.error("Please select checkout address before proceeding.");
+            return;
+        }
+        if (activeStep === 1 && (!selectedUserCheckoutAddress || !paymentMethod)) {
+            toast.error("Please select payment method before proceeding.");
+            return;
+        }
+        setActiveStep((prevStep) => prevStep + 1);
+    };
 
     return (
         <div className={"py-14 min-h-[calc(100vh-100px)]"}>
@@ -24,8 +46,45 @@ const Checkout: React.FC = () => {
                 ))}
             </Stepper>
 
-            <div className={"mt-5"}>
-                {activeStep === 0 && <AddressInfo addresses={addresses}/>}
+            {isLoading || true ? (
+                <div className={"lg:w-[80%] mx-auto py-5"}>
+                    <Skeleton/>
+                </div>
+            ) : (
+                <div className={"mt-5"}>
+                    {activeStep === 0 && <AddressInfo addresses={addresses}/>}
+                </div>
+            )}
+            <div
+                className={`flex justify-between items-center px-4 fixed z-50 h-24 bottom-0 bg-white left-0 
+                    w-full py-4 border-slate-200`}
+                style={{boxShadow: "0 -2px 4px rgba(100, 100, 100, 0.15)"}}>
+                <Button
+                    variant='outlined'
+                    disabled={activeStep === 0}
+                    onClick={handleBack}>
+                    Back
+                </Button>
+
+                {activeStep !== steps.length - 1 && (
+                    <button
+                        disabled={errorMessage || (
+                            activeStep === 0 ? !selectedUserCheckoutAddress
+                                : activeStep === 1 ? !paymentMethod
+                                    : false
+                        )}
+                        className={`bg-custom-blue font-semibold px-6 h-10 rounded-md text-white
+                       ${
+                            errorMessage ||
+                            (activeStep === 0 && !selectedUserCheckoutAddress) ||
+                            (activeStep === 1 && !paymentMethod)
+                                ? "opacity-60"
+                                : ""
+                        }`}
+                        onClick={handleNext}>
+                        Proceed
+                    </button>
+                )}
             </div>
         </div>
     );
