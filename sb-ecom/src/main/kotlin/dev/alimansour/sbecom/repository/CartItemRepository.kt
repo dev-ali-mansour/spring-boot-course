bb1badedd932e8b2d4005716ede716fbb3926c98
Ali Mansour <dev.ali.mansour@gmail.com>
2026-09-06 17:54:07 +0300

    feat: add deleteAllByCartId query to CartItemRepository for bulk deletion by cart ID

--- sb-ecom/src/main/kotlin/dev/alimansour/sbecom/repository/CartItemRepository.kt
+++ sb-ecom/src/main/kotlin/dev/alimansour/sbecom/repository/CartItemRepository.kt
@@ -2,7 +2,6 @@ package dev.alimansour.sbecom.repository
 
 import dev.alimansour.sbecom.model.CartItem
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
@@ -11,4 +10,7 @@ import org.springframework.stereotype.Repository
 interface CartItemRepository : JpaRepository<CartItem, Long> {
     @Query("SELECT ci FROM CartItem ci WHERE ci.cart.id = :cartId AND ci.product.id = :productId")
     fun findCartItemByProductIdAndCartId(@Param("cartId") cartId: Long, @Param("productId") productId: Long): CartItem?
+
+    @Query("DELETE FROM CartItem ci WHERE ci.cart.id = :id")
+    fun deleteAllByCartId(@Param("id") id: Long)
 }
