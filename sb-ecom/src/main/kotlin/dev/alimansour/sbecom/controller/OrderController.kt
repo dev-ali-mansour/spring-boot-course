4eaca36283e055167a9be11b05999a155ff45e7e
Ali Mansour <dev.ali.mansour@gmail.com>
2026-09-08 09:14:20 +0300

    feat: add createStripeClientSecret endpoint to OrderController for payment intent handling

--- sb-ecom/src/main/kotlin/dev/alimansour/sbecom/controller/OrderController.kt
+++ sb-ecom/src/main/kotlin/dev/alimansour/sbecom/controller/OrderController.kt
@@ -1,8 +1,11 @@
 package dev.alimansour.sbecom.controller
 
+import com.stripe.model.PaymentIntent
 import dev.alimansour.sbecom.payload.OrderDTO
 import dev.alimansour.sbecom.payload.OrderRequestDTO
+import dev.alimansour.sbecom.payload.StripePaymentDTO
 import dev.alimansour.sbecom.service.OrderService
+import dev.alimansour.sbecom.service.StripeService
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -11,7 +14,10 @@ import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api")
-class OrderController(private val orderService: OrderService) {
+class OrderController(
+    private val orderService: OrderService,
+    private val stripeService: StripeService
+) {
 
     @Tag(name = "Order APIs", description = "APIs for managing orders")
     @PostMapping("/orders/users/payments/{paymentMethod}")
@@ -21,5 +27,14 @@ class OrderController(private val orderService: OrderService) {
     ): ResponseEntity<OrderDTO> {
         val order: OrderDTO = orderService.placeOrder(paymentMethod, orderRequestDTO)
         return ResponseEntity(order, HttpStatus.CREATED)
+    }
+
+    @Tag(name = "Order APIs", description = "APIs for managing orders")
+    @PostMapping("/orders/stripe-client-secret")
+    fun createStripeClientSecret(
+        @Validated @RequestBody stripePaymentDTO: StripePaymentDTO
+    ): ResponseEntity<String> {
+        val paymentIntent: PaymentIntent = stripeService.paymentIntent(stripePaymentDTO)
+        return ResponseEntity(paymentIntent.clientSecret, HttpStatus.CREATED)
     }
 }
