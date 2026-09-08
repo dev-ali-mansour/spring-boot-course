19c015db0bb409c5561847616311c60aaf6e1b39
Ali Mansour <dev.ali.mansour@gmail.com>
2026-09-08 18:56:57 +0300

    feat: integrate Stripe payment flow with dynamic client secret retrieval

--- ecom-frontend/src/components/checkout/StripePayment.tsx
+++ ecom-frontend/src/components/checkout/StripePayment.tsx
@@ -1,14 +1,61 @@
-import React from "react";
-import {Alert, AlertTitle} from "@mui/material";
+import React, {useMemo} from "react";
+import {useAuthStore, useCartStore} from "@/store";
+import {Elements} from "@stripe/react-stripe-js";
+import {loadStripe} from "@stripe/stripe-js";
+import PaymentForm from "@/components/checkout/PaymentForm";
+import {getErrorMessage, useCreateStripeClientSecret} from "@/hooks/useQueries";
+import Skeleton from "@/components/shared/Skeleton";
+import ErrorPage from "@/components/shared/ErrorPage";
+
+const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
 
 const StripePayment: React.FC = () => {
+    const {user, selectedUserCheckoutAddress} = useAuthStore((state) => state);
+    const {totalPrice} = useCartStore();
+
+    const sendData = useMemo(() => {
+        if (!user || !selectedUserCheckoutAddress) return null;
+
+        return {
+            amount: Math.round(Number(totalPrice) * 100),
+            currency: "usd",
+            email: user.email,
+            name: `${user.firstName} ${user.lastName}`,
+            address: selectedUserCheckoutAddress,
+            description: `Order for ${user.email}`,
+            metadata: {
+                test: "1",
+            },
+        };
+    }, [user, selectedUserCheckoutAddress, totalPrice]);
+
+    const {data, isPending, isError, error} = useCreateStripeClientSecret(sendData);
+    const clientSecret = data?.clientSecret;
+
+    if (isPending) {
+        return (
+            <div className={"max-w-lg mx-auto"}>
+                <Skeleton/>
+            </div>
+        )
+    }
+
+    if (isError) {
+        return (
+            <div className={"max-w-lg mx-auto"}>
+                <ErrorPage message={getErrorMessage(error)} />
+            </div>
+        );
+    }
+
     return (
-        <div className={"h-96 flex justify-center items-center"}>
-            <Alert severity={"warning"} variant={"filled"} style={{maxWidth: "400px"}}>
-                <AlertTitle>Stripe Unavailable</AlertTitle>
-                Stripe payment is unavailable at the moment. Please use another payment method.
-            </Alert>
-        </div>
+        <>
+            {clientSecret && (
+                <Elements stripe={stripePromise} options={{clientSecret}}>
+                    <PaymentForm clientSecret={clientSecret} totalPrice={totalPrice}/>
+                </Elements>
+            )}
+        </>
     );
 };
 
