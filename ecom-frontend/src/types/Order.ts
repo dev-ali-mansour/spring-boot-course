6d41bd9e2911c5d1c8f2fdadff32dc3dc72201a8
Ali Mansour <dev.ali.mansour@gmail.com>
2026-09-10 02:05:30 +0300

    feat: add Order interface to define structure for order details

---
+++ ecom-frontend/src/types/Order.ts
@@ -0,0 +1,19 @@
+import { OrderItem } from "./OrderItem";
+
+export interface Order {
+    id: number | string;
+    email: string;
+    orderItems: OrderItem[];
+    orderDate: string;
+    payment?: {
+        id: number | string;
+        paymentMethod: string;
+        pgPaymentId: string;
+        pgStatus: string;
+        pgResponseMessage: string;
+        pgName: string;
+    };
+    totalAmount: number;
+    orderStatus: string;
+    addressId?: number | string;
+}
