8177855700746d4190d5179bebb69b5db08c5354
Ali Mansour <dev.ali.mansour@gmail.com>
2026-09-09 22:47:14 +0300

    feat: add paginated getAllOrders endpoint for admin users

--- sb-ecom/src/main/kotlin/dev/alimansour/sbecom/controller/OrderController.kt
+++ sb-ecom/src/main/kotlin/dev/alimansour/sbecom/controller/OrderController.kt
@@ -1,12 +1,17 @@
 package dev.alimansour.sbecom.controller
 
 import com.stripe.model.PaymentIntent
+import dev.alimansour.sbecom.config.AppConstants
 import dev.alimansour.sbecom.payload.OrderDTO
 import dev.alimansour.sbecom.payload.OrderRequestDTO
+import dev.alimansour.sbecom.payload.OrderResponse
 import dev.alimansour.sbecom.payload.StripePaymentDTO
 import dev.alimansour.sbecom.service.OrderService
 import dev.alimansour.sbecom.service.StripeService
 import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Sort
+import org.springframework.data.web.PageableDefault
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.annotation.Validated
@@ -36,5 +41,18 @@ class OrderController(
     ): ResponseEntity<String> {
         val paymentIntent: PaymentIntent = stripeService.createPaymentIntent(stripePaymentDTO)
         return ResponseEntity(paymentIntent.clientSecret, HttpStatus.CREATED)
+    }
+
+    @GetMapping("/admin/orders")
+    fun getAllOrders(
+        @PageableDefault(
+            page = AppConstants.PAGE_NUMBER,
+            size = AppConstants.PAGE_SIZE,
+            sort = [AppConstants.SORT_ORDERS_BY],
+            direction = Sort.Direction.ASC
+        ) pageable: Pageable,
+    ): ResponseEntity<OrderResponse> {
+        val orders: OrderResponse = orderService.getAllOrders(pageable)
+        return ResponseEntity(orders, HttpStatus.OK)
     }
 }
