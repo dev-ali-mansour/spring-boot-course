0c4b3d08a9e64c3c5e26970a2debc76c5be2484c
Ali Mansour <dev.ali.mansour@gmail.com>
2026-09-09 06:33:53 +0300

    feat: add additional fields to StripePaymentDTO including email, name, address, description, and metadata

--- sb-ecom/src/main/kotlin/dev/alimansour/sbecom/payload/StripePaymentDTO.kt
+++ sb-ecom/src/main/kotlin/dev/alimansour/sbecom/payload/StripePaymentDTO.kt
@@ -1,6 +1,13 @@
 package dev.alimansour.sbecom.payload
 
+import dev.alimansour.sbecom.model.Address
+
 data class StripePaymentDTO(
     val amount: Long,
     val currency: String,
+    val email: String,
+    val name: String,
+    val address: Address,
+    val description: String,
+    val metadata: Map<String, String>,
 )
