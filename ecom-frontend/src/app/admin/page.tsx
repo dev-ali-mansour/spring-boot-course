965e6f3b57334a7bb19f7f5206030abd812a3f96
Ali Mansour <dev.ali.mansour@gmail.com>
2026-09-09 14:17:49 +0300

    feat: add Admin Dashboard page with metadata and Dashboard component rendering

---
+++ ecom-frontend/src/app/admin/page.tsx
@@ -0,0 +1,13 @@
+import Dashboard from "@/components/admin/dashboard/Dashboard";
+
+export const metadata = {
+    title: "Admin Dashboard",
+    description: "Manage your e-commerce platform with ease using the Admin Dashboard.",
+};
+
+export default function AdminDashboardPage() {
+    return (
+        <Dashboard/>
+    );
+}
+
