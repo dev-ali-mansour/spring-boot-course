aacf34b74cdc75d1739c43de885025fbe4778601
Ali Mansour <dev.ali.mansour@gmail.com>
2026-09-09 14:18:10 +0300

    feat: add Orders component for admin panel

---
+++ ecom-frontend/src/components/admin/oreders/Orders.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Orders: React.FC = () => {
+    return (
+        <div>
+            <h1>Orders</h1>
+            <p>Manage your orders in the Admin Panel.</p>
+        </div>
+    );
+};
+
+export default Orders;
