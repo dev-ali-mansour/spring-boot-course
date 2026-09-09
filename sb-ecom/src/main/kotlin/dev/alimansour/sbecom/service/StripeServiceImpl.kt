a7c122d6ec05bbd2887213a7beb3a30f72df811f
Ali Mansour <dev.ali.mansour@gmail.com>
2026-09-09 06:35:35 +0300

    feat: add support for Stripe customer creation and association in payment workflow

--- sb-ecom/src/main/kotlin/dev/alimansour/sbecom/service/StripeServiceImpl.kt
+++ sb-ecom/src/main/kotlin/dev/alimansour/sbecom/service/StripeServiceImpl.kt
@@ -1,7 +1,10 @@
 package dev.alimansour.sbecom.service
 
 import com.stripe.StripeClient
+import com.stripe.model.Customer
 import com.stripe.model.PaymentIntent
+import com.stripe.param.CustomerCreateParams
+import com.stripe.param.CustomerSearchParams
 import com.stripe.param.PaymentIntentCreateParams
 import dev.alimansour.sbecom.payload.StripePaymentDTO
 import jakarta.annotation.PostConstruct
@@ -22,10 +25,13 @@ class StripeServiceImpl(
     }
 
     override fun createPaymentIntent(stripePaymentDTO: StripePaymentDTO): PaymentIntent {
+        val customer: Customer? = findOrCreateCustomer(stripePaymentDTO)
+
         val params =
             PaymentIntentCreateParams.builder()
                 .setAmount(stripePaymentDTO.amount)
                 .setCurrency(stripePaymentDTO.currency)
+                .setCustomer(customer?.id)
                 .setAutomaticPaymentMethods(
                     PaymentIntentCreateParams.AutomaticPaymentMethods.builder()
                         .setEnabled(true)
@@ -36,5 +42,32 @@ class StripeServiceImpl(
         val paymentIntent = client.v1().paymentIntents().create(params)
 
         return paymentIntent
+    }
+
+    private fun findOrCreateCustomer(stripePaymentDTO: StripePaymentDTO): Customer? {
+        val searchParams =
+            CustomerSearchParams.builder()
+                .setQuery("email:'${stripePaymentDTO.email}'")
+                .build()
+        val stripeSearchResult = client.v1().customers().search(searchParams)
+        if (stripeSearchResult.data.isEmpty()) {
+            val params =
+                CustomerCreateParams.builder()
+                    .setName(stripePaymentDTO.name)
+                    .setEmail(stripePaymentDTO.email)
+                    .setAddress(
+                        CustomerCreateParams.Address.builder()
+                            .setLine1(stripePaymentDTO.address.street)
+                            .setCity(stripePaymentDTO.address.city)
+                            .setState(stripePaymentDTO.address.state)
+                            .setPostalCode(stripePaymentDTO.address.pinCode)
+                            .setCountry(stripePaymentDTO.address.country)
+                            .build()
+                    )
+                    .build()
+            return client.v1().customers().create(params)
+        } else {
+            return stripeSearchResult.data[0]
+        }
     }
 }
