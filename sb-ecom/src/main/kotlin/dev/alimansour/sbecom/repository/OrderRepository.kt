6950fa3409dc49ea9c3adeee2d1182a65ebb2a17
Ali Mansour <dev.ali.mansour@gmail.com>
2026-09-09 15:43:01 +0300

    feat: add getTotalRevenue query method to OrderRepository

--- sb-ecom/src/main/kotlin/dev/alimansour/sbecom/repository/OrderRepository.kt
+++ sb-ecom/src/main/kotlin/dev/alimansour/sbecom/repository/OrderRepository.kt
@@ -2,7 +2,11 @@ package dev.alimansour.sbecom.repository
 
 import dev.alimansour.sbecom.model.Order
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 
 @Repository
-interface OrderRepository : JpaRepository<Order, Long>
+interface OrderRepository : JpaRepository<Order, Long> {
+    @Query("SELECT COALESCE( SUM(o.totalAmount),0) FROM Order o")
+    fun getTotalRevenue(): Double
+}
