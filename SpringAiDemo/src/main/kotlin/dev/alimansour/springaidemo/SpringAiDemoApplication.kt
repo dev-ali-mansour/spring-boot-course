e6b0c5ee6eaffa517649dafc3a64e73e3423f146
Ali Mansour <dev.ali.mansour@gmail.com>
2026-09-12 01:27:32 +0300

    feat: initialize Spring Boot Kotlin project (SpringAiDemo) with Maven wrapper and required dependencies

---
+++ SpringAiDemo/src/main/kotlin/dev/alimansour/springaidemo/SpringAiDemoApplication.kt
@@ -0,0 +1,11 @@
+package dev.alimansour.springaidemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringAiDemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringAiDemoApplication>(*args)
+}
---
+++ SpringAiDemo/src/test/kotlin/dev/alimansour/springaidemo/SpringAiDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.alimansour.springaidemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringAiDemoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
