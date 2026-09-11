1a55434617d5ae7d84fd895e91f2d760a9e2d574
Ali Mansour <dev.ali.mansour@gmail.com>
2026-09-12 00:03:46 +0300

    feat: add findByUser method to ProductRepository for retrieving user-specific products

--- sb-ecom/src/main/kotlin/dev/alimansour/sbecom/repository/ProductRepository.kt
+++ sb-ecom/src/main/kotlin/dev/alimansour/sbecom/repository/ProductRepository.kt
@@ -2,6 +2,7 @@ package dev.alimansour.sbecom.repository
 
 import dev.alimansour.sbecom.model.Category
 import dev.alimansour.sbecom.model.Product
+import dev.alimansour.sbecom.model.User
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
@@ -12,4 +13,5 @@ import org.springframework.stereotype.Repository
 interface ProductRepository : JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
     fun findByCategory(category: Category, pageable: Pageable): Page<Product>
     fun findByNameLikeIgnoreCase(name: String, pageable: Pageable): Page<Product>
+    fun findByUser(user: User, pageable: Pageable): Page<Product>
 }
