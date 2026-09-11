f6d5f1e82763816ff7bae1181dfd04c4859fd42f
Ali Mansour <dev.ali.mansour@gmail.com>
2026-09-11 14:11:53 +0300

    feat: add findByRoleName query in UserRepository for filtering users by role

--- sb-ecom/src/main/kotlin/dev/alimansour/sbecom/repository/UserRepository.kt
+++ sb-ecom/src/main/kotlin/dev/alimansour/sbecom/repository/UserRepository.kt
@@ -1,7 +1,11 @@
 package dev.alimansour.sbecom.repository
 
+import dev.alimansour.sbecom.model.AppRole
 import dev.alimansour.sbecom.model.User
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 import java.util.*
 
@@ -10,4 +14,7 @@ interface UserRepository : JpaRepository<User, Long> {
     fun findByUsername(username: String): Optional<User>
     fun existsByUsername(username: String): Boolean
     fun existsByEmail(email: String): Boolean
+
+    @Query("SELECT u FROM User u JOIN u.roles r WHERE r.name=:role")
+    fun findByRoleName(role: AppRole, pageable: Pageable): Page<User>
 }
