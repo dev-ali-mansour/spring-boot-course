2ab76c485c0ed91b47ba9a172c39099f167510e0
Ali Mansour <dev.ali.mansour@gmail.com>
2026-09-11 13:25:30 +0300

    refactor: delegate authentication logic to AuthService for cleaner and more maintainable AuthController

--- sb-ecom/src/main/kotlin/dev/alimansour/sbecom/controller/AuthController.kt
+++ sb-ecom/src/main/kotlin/dev/alimansour/sbecom/controller/AuthController.kt
@@ -1,172 +1,55 @@
 package dev.alimansour.sbecom.controller
 
-import dev.alimansour.sbecom.model.AppRole
-import dev.alimansour.sbecom.model.Role
-import dev.alimansour.sbecom.model.User
-import dev.alimansour.sbecom.repository.RoleRepository
-import dev.alimansour.sbecom.repository.UserRepository
-import dev.alimansour.sbecom.security.jwt.JwtUtils
 import dev.alimansour.sbecom.security.request.SignInRequest
 import dev.alimansour.sbecom.security.request.SignUpRequest
 import dev.alimansour.sbecom.security.response.MessageResponse
 import dev.alimansour.sbecom.security.response.UserInfoResponse
-import dev.alimansour.sbecom.security.service.UserDetailsImpl
+import dev.alimansour.sbecom.service.AuthService
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.HttpHeaders
-import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseCookie
 import org.springframework.http.ResponseEntity
-import org.springframework.security.authentication.AuthenticationManager
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
-import org.springframework.security.core.Authentication
-import org.springframework.security.core.AuthenticationException
-import org.springframework.security.core.context.SecurityContextHolder
-import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.http.ResponseEntity.ok
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.*
-import java.util.stream.Collectors
 
 @RestController
 @RequestMapping(value = ["/api/auth"])
-class AuthController(
-    private val authenticationManager: AuthenticationManager,
-    private val jwtUtils: JwtUtils,
-    private val userRepository: UserRepository,
-    private val roleRepository: RoleRepository,
-    private val encoder: PasswordEncoder,
-) {
+class AuthController(private val authService: AuthService) {
 
     @Tag(name = "Authentication APIs", description = "APIs for user authentication")
-    @PostMapping("/signin")
-    fun authenticateUser(@Validated @RequestBody signInRequest: SignInRequest): ResponseEntity<Any> {
-        val authentication: Authentication
-        try {
-            authentication = authenticationManager.authenticate(
-                UsernamePasswordAuthenticationToken(
-                    signInRequest.username,
-                    signInRequest.password
-                )
-            )
-        } catch (_: AuthenticationException) {
-            val map: MutableMap<String, Any> = mutableMapOf()
-            map["message"] = "Bad credentials"
-            map["status"] = false
-            return ResponseEntity(map, HttpStatus.UNAUTHORIZED)
-        }
-
-        SecurityContextHolder.getContext().authentication = authentication
-        val userDetails: UserDetailsImpl = authentication.principal as UserDetailsImpl
-        val jwtCookie: ResponseCookie = jwtUtils.generateJwtCookie(userDetails)
-        val roles: List<String> = userDetails.authorities.stream()
-            .map { item -> item.authority.orEmpty() }
-            .collect(Collectors.toList())
-
-        val response = UserInfoResponse(
-            id = userDetails.id,
-            firstName = userDetails.firstName,
-            lastName = userDetails.lastName,
-            jwtToken = jwtCookie.value,
-            username = userDetails.username,
-            email = userDetails.email,
-            roles = roles
-        )
-
-        return ResponseEntity.ok()
-            .header(HttpHeaders.SET_COOKIE, jwtCookie.toString())
-            .body(response)
+    @PostMapping("/login")
+    fun authenticateUser(@Validated @RequestBody signInRequest: SignInRequest): ResponseEntity<UserInfoResponse> {
+        val result = authService.login(signInRequest)
+        return ok()
+            .header(HttpHeaders.SET_COOKIE, result.jwtCookie.toString())
+            .body(result.userInfoResponse)
     }
 
     @Tag(name = "Authentication APIs", description = "APIs for user authentication")
-    @PostMapping("/signup")
+    @PostMapping("/register")
     fun registerUser(@Validated @RequestBody signUpRequest: SignUpRequest): ResponseEntity<Any> {
-        if (userRepository.existsByUsername(signUpRequest.username)) {
-            return ResponseEntity
-                .badRequest()
-                .body(MessageResponse("Error: username already taken!"))
-        }
-
-        if (userRepository.existsByEmail(signUpRequest.email)) {
-            return ResponseEntity
-                .badRequest()
-                .body(MessageResponse("Error: email already taken!"))
-        }
-
-        val user = User(
-            firstName = signUpRequest.firstName,
-            lastName = signUpRequest.lastName,
-            username = signUpRequest.username,
-            email = signUpRequest.email,
-            password = encoder.encode(signUpRequest.password)
-                ?: throw RuntimeException("Error: password missing!"),
-        )
-
-        val roles: MutableSet<Role> = hashSetOf()
-        signUpRequest.roles?.let { strRoles ->
-            strRoles.forEach { role ->
-                when (role) {
-                    "admin" -> {
-                        val adminRole = roleRepository.findByName(AppRole.ROLE_ADMIN)
-                            .orElseThrow { RuntimeException("Error: Roles is not found!") }
-                        roles.add(adminRole)
-                    }
-
-                    "seller" -> {
-                        val sellerRole = roleRepository.findByName(AppRole.ROLE_SELLER)
-                            .orElseThrow { RuntimeException("Error: Roles is not found!") }
-                        roles.add(sellerRole)
-                    }
-
-                    else -> {
-                        val userRole = roleRepository.findByName(AppRole.ROLE_USER)
-                            .orElseThrow { RuntimeException("Error: Roles is not found!") }
-                        roles.add(userRole)
-                    }
-                }
-            }
-
-        } ?: run {
-            val userRole = roleRepository.findByName(AppRole.ROLE_USER)
-                .orElseThrow { RuntimeException("Error: Roles is not found!") }
-            roles.add(userRole)
-        }
-        user.roles = roles
-        userRepository.save(user)
-
-        return ResponseEntity.ok(MessageResponse("User registered successfully"))
+        authService.register(signUpRequest)
+        return ok(MessageResponse("User registered successfully"))
     }
 
     @Tag(name = "Authentication APIs", description = "APIs for user authentication")
     @GetMapping("/username")
-    fun currentUsername(authentication: Authentication?): String =
-        authentication?.name.orEmpty()
+    fun currentUsername(): String = authService.getCurrentUsername()
 
     @Tag(name = "Authentication APIs", description = "APIs for user authentication")
     @GetMapping("/user")
-    fun getUserDetails(authentication: Authentication?): ResponseEntity<UserInfoResponse> {
-        val userDetails: UserDetailsImpl = authentication?.principal as UserDetailsImpl
-        val roles: List<String> = userDetails.authorities.stream()
-            .map { item -> item.authority.orEmpty() }
-            .collect(Collectors.toList())
-
-        val response = UserInfoResponse(
-            id = userDetails.id,
-            firstName = userDetails.firstName,
-            lastName = userDetails.lastName,
-            username = userDetails.username,
-            email = userDetails.email,
-            roles = roles
-        )
-
-        return ResponseEntity.ok().body(response)
+    fun getUserDetails(): ResponseEntity<UserInfoResponse> {
+        val response = authService.getUserInfo()
+        return ok().body(response)
     }
 
     @Tag(name = "Authentication APIs", description = "APIs for user authentication")
-    @PostMapping("/signout")
-    fun signOutUser(): ResponseEntity<MessageResponse> {
-        val cookie = jwtUtils.getCleanJwtCookie()
+    @PostMapping("/logout")
+    fun logoutUser(): ResponseEntity<MessageResponse> {
+        val cookie = authService.logout()
 
-        return ResponseEntity.ok()
+        return ok()
             .header(HttpHeaders.SET_COOKIE, cookie.toString())
-            .body(MessageResponse("You've been signed out!"))
+            .body(MessageResponse("You've been logged out!"))
     }
 }
